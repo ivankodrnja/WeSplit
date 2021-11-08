7e3af2693a4768ebc04f2fffbb1588a9cc74734c
Ivan Kodrnja <ikodrnja@gmail.com>
2021-11-08 19:31:13 +0100

    Added a header to the third section, saying “Amount per person” Added another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people. Added functionality to change the tip percentage with a range from 0% to 100%

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -44,17 +44,35 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage){
-                        ForEach(tipPercentages, id:\.self){
+                        ForEach(0..<101){
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage){
+//                        ForEach(tipPercentages, id:\.self){
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                } header: {
+//                    Text("How much tip do you want to leave?")
+//                }
+                
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section {
+                    Text(checkAmount + checkAmount / 100 * Double(tipPercentage), format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total amount")
                 }
             }
             .navigationTitle("WeSplit")
